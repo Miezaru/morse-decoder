ea674844b943bd1fb54bce19a6862295321082f6
Artem Usatyi <badlywaves@gmail.com>
2020-10-03 11:23:29 +0300

    init: solve

--- src/index.js
+++ src/index.js
@@ -1,46 +1,73 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    // write your solution here
+  let letter = "";
+  let example = "";
+  let finalMessage = "";
+  for (let i = 10; i <= expr.length; i += 10) {
+    example = "";
+    letter = expr.slice(i - 10, i);
+
+    if (letter.includes("*")) {
+      finalMessage += " ";
+      continue;
+    } else {
+      for (let j = 0; j < letter.length; j += 2) {
+        if (letter[j] + letter[j + 1] === "10") {
+          example += ".";
+        } else if (letter[j] + letter[j + 1] === "11") {
+          example += "-";
+        } else if (letter[j] + letter[j + 1] === "00") {
+          continue;
+        }
+      }
+
+      for (const [key, value] of Object.entries(MORSE_TABLE)) {
+        if (key === example) finalMessage += value;
+      }
+    }
+  }
+  console.log(finalMessage);
+  return finalMessage;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
